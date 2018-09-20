b5b8644b44568d0051858d048dc4a7707bfc0670
afmoreno10 <af.moreno10@uniandes.edu.co>
2018-09-20 09:06:11 -0500

    First commit

--- README.md
+++ README.md
@@ -8,18 +8,18 @@ This is the project made to be used in the creation and development of the first
 ### Running the back-end:
 The back-end of the project is supposed to be running at port 3001. It is already set up in the ./bin/www file.
 
-To start the back, open the midterm folder and execute the following command lines:
+To start the back, open the project and execute the following command lines:
 `yarn add`
 `yarn start`
 
 ### Running the front-extended
 The front-end is expected to be running at port 3000 as it is the default port used by React.
 
-To start the front, go to midterm1/front and execute the following command lines:
+To start the front, go to ./front and execute the following command lines:
 `yarn add`
 `yarn start`
 
 ### Preparing the data:
 
 `cd/data
-mongoimport -d <name of the database (you create this name)> -c <name of the collection (you create this name)> --jsonArray --file <name of the existing file to extract the data>`
+mongoimport -d <name of the database (you create this name)> -c <name of the collection (you create this name)> --jsonArray (--type csv) --file <name of the existing file to extract the data> --headerline`
--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
+import vegaEmbed from "vega-embed";
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      spec : {
+        "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+        "description": "A simple bar chart with embedded data.",
+        "data": {
+          "myData": [
+            {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
+            {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
+            {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
+          ]
+        },
+        "mark": "bar",
+        "encoding": {
+          "y": {"field": "a", "type": "ordinal"},
+          "x": {"field": "b", "type": "quantitative"}
+        }
+      }
+    };
+  }
+
   render() {
     return (
       <div className="App">
 
-      </div>
-    );
-  }
+        <textarea
+          cols="40"
+          rows="20"
+          ref={(div) => this.divTarget=div}>
+          Soy un div
+        </textarea>
+
+        <button onClick={() => {
+
+          // var obj = {
+          //   x: "abcd",
+          //   y: {"field": "defg", "type": "quantitative"}
+
+          this.state.spec
+        };
+
+        this.divTarget.value=JSON.stringify(obj, null, 2);
+      }}> Cambialo </button>
+    </div>
+  );
+}
 }
 
 export default App;
