d7a7a22880f161b695267c99db74e7342cc9b7d8
afmoreno10 <af.moreno10@uniandes.edu.co>
2018-09-22 06:34:14 -0500

    Shows graph with static data

--- README.md
+++ README.md
@@ -9,14 +9,14 @@ This is the project made to be used in the creation and development of the first
 The back-end of the project is supposed to be running at port 3001. It is already set up in the ./bin/www file.
 
 To start the back, open the project and execute the following command lines:
-`yarn add`
+`yarn install`
 `yarn start`
 
 ### Running the front-extended
 The front-end is expected to be running at port 3000 as it is the default port used by React.
 
 To start the front, go to ./front and execute the following command lines:
-`yarn add`
+`yarn install`
 `yarn start`
 
 ### Preparing the data:
--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react';
-import vegaEmbed from "vega-embed";
 import './App.css';
+import vegaEmbed from 'vega-embed';
 
 class App extends Component {
+
   constructor(props){
     super(props);
+  }
 
-    this.state={
-      spec : {
-        "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
-        "description": "A simple bar chart with embedded data.",
-        "data": {
-          "values": [
-            {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
-            {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
-            {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
-          ]
-        },
-        "mark": "bar",
-        "encoding": {
-          "y": {"field": "a", "type": "ordinal"},
-          "x": {"field": "b", "type": "quantitative"}
-        }
+  state = {
+    spec : ["tema 1", "tema 2", "tema 3"],
+    values : [
+      {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
+      {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
+      {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
+    ]
+  }
+
+  // this.changeHandler = this.changeHandler.bind(this);
+
+
+  changeHandler(event){
+
+  }
+
+  createGraph(){
+    var myData = this.state.values;
+
+    var spec = {
+      "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+      "description": "A simple bar chart with embedded data.",
+      "data": {
+        "name": "myData"
+      },
+      "mark": "bar",
+      "encoding": {
+        "y": {"field": "a", "type": "ordinal"},
+        "x": {"field": "b", "type": "quantitative"}
       }
     };
+
+    const embed_opt = {"mode": "vega-lite"};
+    const el = document.getElementById('vis');
+    const view = vegaEmbed("#vis", spec, embed_opt)
+    .catch(error => console.log("Failure to insert graphic"))
+    .then((res) =>  res.view.insert("myData", myData).run());
+
   }
 
   render() {
@@ -39,18 +61,21 @@ class App extends Component {
 
         <button onClick={() => {
 
-          // var obj = {
-          //   x: "abcd",
-          //   y: {"field": "defg", "type": "quantitative"}
+          var obj = {
+            x: "abcd",
+            y: {"field": "defg", "type": "quantitative"}
+          };
+          // this.setState({spec});
+          this.divTarget.value=JSON.stringify(obj, null, 2);
+        }
 
-          this.state.spec
-        };
+      }> Cambialo </button>
+      <button className="btn btn-primary" onClick={() =>  this.createGraph()}>Graph</button>
+      <div id="vis"></div>
+      </div>
+    );
+  }
+}
 
-        this.divTarget.value=JSON.stringify(obj, null, 2);
-      }}> Cambialo </button>
-    </div>
-  );
-}
-}
 
 export default App;
